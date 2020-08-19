94c0e312d3aafe0768be6cdfd054015265d08eab
Jason Easterly <jason.easterly11@gmail.com>
2020-08-19 00:09:34 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,8 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |value_to_find| {
+  count = 0
+  while count < array.length do
     array.include?(value_to_find)
-  }
-  array.index
+  end
+  if value_to_find
+    array.find_element_index(value_to_find)
 end
