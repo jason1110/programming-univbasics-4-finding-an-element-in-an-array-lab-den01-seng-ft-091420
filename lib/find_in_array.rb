613308367667161eb050a88d662a2acb8bb7fd85
Jason Easterly <jason.easterly11@gmail.com>
2020-08-19 00:10:19 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,5 +4,5 @@ def find_element_index(array, value_to_find)
     array.include?(value_to_find)
   end
   if value_to_find
-    array.find_element_index(value_to_find)
+    array.index(value_to_find)
 end
